b03cbc66ebe0cdcc76c9744907345d9db1612c3f
Matheus Schmitz <schmitz.tech.consulting@gmail.com>
2025-07-21 09:54:30 -0300

    feat: creating base project with spring initializr

---
+++ src/main/java/com/gmt/gmttechchallenge/GmtTechChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.gmt.gmttechchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmtTechChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmtTechChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmt/gmttechchallenge/GmtTechChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmt.gmttechchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmtTechChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
