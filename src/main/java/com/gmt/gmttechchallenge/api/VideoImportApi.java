45b10fa8ffca3d2f45b0c5c7c2889d95e1e3634b
Matheus Schmitz <schmitz.tech.consulting@gmail.com>
2025-07-22 14:50:33 -0300

    feat: add JWT lib to be used for auth

--- src/main/java/com/gmt/gmttechchallenge/api/VideoImportApi.java
+++ src/main/java/com/gmt/gmttechchallenge/api/VideoImportApi.java
@@ -2,18 +2,19 @@ package com.gmt.gmttechchallenge.api;
 
 import com.gmt.gmttechchallenge.services.VideoImportService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller("/videos/import")
+@RestController
 @RequiredArgsConstructor
 public class VideoImportApi {
 
     private final VideoImportService videoImportService;
 
     // TODO add spring validation
-    @PostMapping
-    void importVideos(VideosImport videos){
+    @PostMapping("/videos/import")
+    void importVideos(@RequestBody VideosImport videos){
         videoImportService.importVideoMetadata(videos);
     }
 }
